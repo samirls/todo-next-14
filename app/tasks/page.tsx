a150b453b90f98da951ce6a8de2bd5979423cd26
samirls <samirlaguardiaii@gmail.com>
2023-12-14 00:40:24 -0300

    added condicional rendering to empty tasks

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -3,12 +3,15 @@ import { fetchTasks } from "../lib/data";
 import { Box, Button, useDisclosure } from "@chakra-ui/react";
 import Task from "./Task";
 import AddTaskModal from "./modals/AddTaskModal";
+import { GiCardboardBox } from "react-icons/gi";
 
 async function Tasks() {
   const fetchedTasks = await fetchTasks();
 
+  console.log(fetchedTasks);
+
   return (
-    <Box pb={{base: '110px', lg:"150px"}}>
+    <Box pb={{ base: "110px", lg: "150px" }}>
       <Box
         fontSize="2rem"
         fontWeight="500"
@@ -18,13 +21,21 @@ async function Tasks() {
       >
         Tasks
       </Box>
-      <Box pl={{base: '10px', lg:'100px'}} py='10px'>
+      <Box pl={{ base: "10px", lg: "100px" }} py="10px">
         <AddTaskModal />
       </Box>
-      <Box >
+      <Box>
         {fetchedTasks.map((task) => (
           <Task key={task.task_id} task={task} />
         ))}
+        {fetchedTasks.length === 0 && (
+          <Box display='flex' justifyContent='center' alignItems='center' flexDir='column' height='400px'>
+            <Box fontSize="4rem">
+              <GiCardboardBox />
+            </Box>
+            <Box>Empty</Box>
+          </Box>
+        )}
       </Box>
     </Box>
   );
